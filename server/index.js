45f779b48c25bf0306bafe52211910d94b25386c
relish <guptarelish333@gmail.com>
2024-06-30 02:15:44 +0530

    updated cors

--- server/index.js
+++ server/index.js
@@ -10,6 +10,13 @@ require("dotenv").config();
 app.use(cors());
 app.use(express.json());
 
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', 'https://chat-app-j744.vercel.app');
+    res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+    res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+    next();
+  });
+
 app.use("/api/auth",userRoutes);
 app.use("/api/messages",messageRoutes);
 
