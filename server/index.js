c926eafa9406a81323477ffdbeeab882a6767abc
relish <guptarelish333@gmail.com>
2024-06-30 02:29:59 +0530

    updated cors

--- server/index.js
+++ server/index.js
@@ -9,7 +9,7 @@ require("dotenv").config();
 
 const corsOptions = {
     origin: 'https://chat-app-j744.vercel.app',
-    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+    methods: ['GET', 'POST', 'OPTIONS'],
     allowedHeaders: ['Content-Type', 'Authorization']
   };
 
@@ -20,7 +20,7 @@ app.use(express.json());
 
 app.use((req, res, next) => {
     res.header('Access-Control-Allow-Origin', 'https://chat-app-j744.vercel.app');
-    res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+    res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
     res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
     res.header('Access-Control-Max-Age', '86400'); // Add this line to specify the max age for CORS
     next();
